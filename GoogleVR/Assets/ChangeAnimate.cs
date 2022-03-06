9c3c611a0cab17ac2a523a806c60995b8aaa4fdd
oatchara0612 <jar.tukaew1907@gmail.com>
2022-03-06 22:21:45 +0700

    first commit

---
+++ GoogleVR/Assets/ChangeAnimate.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeAnimate : MonoBehaviour
+{
+  public void getAnimatorDencing()
+    {
+        this.GetComponent<Animator>().Play("Defeated");
+    }
+  public void getAnimatorRumba()
+    {
+        this.GetComponent<Animator>().Play("Twist Dance");
+    }
+}
